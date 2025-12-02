5479758c0638c16b6d0f96048322c239ec595a33
Hugo Casas <155012694+thebonejuice@users.noreply.github.com>
2025-12-02 16:46:02 -0600

    Add a prototype of the grid.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const containerDiv = document.createElement("div");
 containerDiv.classList.add("container");
 
-containerDiv.textContent="Hello world!";
-
 document.body.appendChild(containerDiv);
+
+for(i = 0; i < 16; i++){
+    const row = document.createElement("div");
+    containerDiv.appendChild(row);
+    row.classList.add("row");
+    for(j = 0; j < 16; j++){
+        const col = document.createElement("div");
+        row.appendChild(col);
+        col.classList.add("col");
+        col.textContent= "column";
+    }
+}
