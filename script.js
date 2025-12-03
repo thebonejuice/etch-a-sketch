26b07e0e538e2db1b5a720fd228b309e54f3e7b3
Hugo Casas <155012694+thebonejuice@users.noreply.github.com>
2025-12-03 13:04:15 -0600

    Fix the grid to a usable state and project ready state

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ containerDiv.classList.add("container");
 
 document.body.appendChild(containerDiv);
 
-for(i = 0; i < 16; i++){
+for(i = 0; i < 15; i++){
     const row = document.createElement("div");
     containerDiv.appendChild(row);
     row.classList.add("row");
-    for(j = 0; j < 16; j++){
+    for(j = 0; j < 15; j++){
         const col = document.createElement("div");
         row.appendChild(col);
         col.classList.add("col");
-        col.textContent= "column";
+        col.textContent= ".";
     }
 }
+
